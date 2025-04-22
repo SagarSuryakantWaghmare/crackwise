c8450b6966b2dbe59ba525a5ab8ead7d7fccff4d
Sagar <sagarwaghmare1384@gmail.com>
2025-04-22 16:54:57 +0530

    Error in the userid

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,7 +4,21 @@ import React from 'react'
 import Image from 'next/image'
 import { dummyInterviews } from '@/constants'
 import InterviewCard from '@/components/InterviewCard'
-const page = () => {
+import { getCurrentUser } from '@/lib/actions/auth.action';
+import { getInterviewsByUserId,getLatestInterviews } from '@/lib/actions/auth.action'
+const page = async() => {
+  const user = await getCurrentUser();
+  // From the cookie get the user id and get the user interviews
+  
+
+
+  const [userInterviews, allInterview] = await Promise.all([
+    getInterviewsByUserId(user?.id!),
+    getLatestInterviews({ userId: user?.id! }),
+  ]);
+
+  const hasPastInterviews = userInterviews?.length! > 0;
+  const hasUpcomingInterviews = allInterview?.length! > 0;
   return (
     <>
       <section className='card-cta'>
@@ -21,22 +35,43 @@ const page = () => {
       </section>
       <section className='flex flex-col gap-6 mt-8'>
         <h2>Your Interviews</h2>
-        <div className='interviews-section'>
-          {dummyInterviews.map((interview) => (
-            <InterviewCard {...interview} key={interview.id} />
-          ))}
-          {/* ' => &apos; */}
-          {/* <p>You haven't taken any interviews yet</p> */}
+        <div className="interviews-section">
+          {hasPastInterviews ? (
+            userInterviews?.map((interview) => (
+              <InterviewCard
+                key={interview.id}
+                userId={user?.id}
+                interviewId={interview.id}
+                role={interview.role}
+                type={interview.type}
+                techstack={interview.techstack}
+                createdAt={interview.createdAt}
+              />
+            ))
+          ) : (
+            <p>You haven&apos;t taken any interviews yet</p>
+          )}
         </div>
         <section className='flex flex-col gap-6 mt-8'>
 
           <h2>Taken an Interview</h2>
-          <div className='interviews-section'>
-            {dummyInterviews.map((interview,index) => (
-              <InterviewCard {...interview} key={index} />
-            ))}
-            {/* <p>There are no interviews available</p> */}
-          </div>
+          <div className="interviews-section">
+          {hasUpcomingInterviews ? (
+            allInterview?.map((interview) => (
+              <InterviewCard
+                key={interview.id}
+                userId={user?.id}
+                interviewId={interview.id}
+                role={interview.role}
+                type={interview.type}
+                techstack={interview.techstack}
+                createdAt={interview.createdAt}
+              />
+            ))
+          ) : (
+            <p>There are no interviews available</p>
+          )}
+        </div>
 
         </section>
       </section>
